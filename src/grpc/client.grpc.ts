eddc3d5e4884023cabafe798373105e252639745
giangn <GiangN>
2025-09-12 13:03:21 +0700

    removed grpc

--- src/grpc/client.grpc.ts
+++
@@ -1,9 +0,0 @@
-import { credentials } from '@grpc/grpc-js';
-import { AuthServiceClient } from '../generated/auth';
-
-const authServiceClient = new AuthServiceClient(
-    process.env.AUTH_GRPC_URL || 'localhost:50051',
-    credentials.createInsecure()
-);
-
-export default authServiceClient;
