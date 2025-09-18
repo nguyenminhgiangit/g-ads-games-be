7ca0f50fb4525c459192a6d64dedca06e3553cde
giangn <GiangN>
2025-09-18 09:27:02 +0700

    [api] get | path - clean code

--- src/routers/admin.routes.ts
+++ src/routers/admin.routes.ts
@@ -5,7 +5,6 @@ import { authMiddleware } from "../middlewares/auth.middleware";
 
 const adminRouters = Router();
 
-// adminRouters.patch('/user/:id', authMiddleware, requireAdminRole, adminController.updateUser);
 adminRouters.get('/config', authMiddleware, requireAdminRole, adminController.getConfig);
 adminRouters.patch('/config', authMiddleware, requireAdminRole, adminController.updateConfig);
 
